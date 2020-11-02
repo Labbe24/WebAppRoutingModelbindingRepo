aca8660bed76221d723e7bb92e079ee2593d4b3c
Lasse Andresen <lasseandresen.1996@hotmail.com>
2020-11-02 11:29:18 +0100

    Add project files.

---
+++ WebAppRoutingModelbinding/Controllers/Lab1Controller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppRoutingModelbinding.Controllers
+{
+    public class Lab1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowResult(int x = 2, int y = 4)
+        {
+            return Content(string.Format($"x={x}, og y={y}"));
+        }
+    }
+}
